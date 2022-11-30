ad713011f2f5e0304001f64ff4af33d972f70e7d
pedrofn <pedrofechado@gmail.com>
2022-11-30 18:46:26 +0000

    created basic fetch to get allProducts from API

---
+++ src/utils/apiRequests.js
@@ -0,0 +1,12 @@
+export const extractResponseData = (response) => {
+    return response.json();
+}
+
+export const fetchProducts = async () => {
+    const response = await fetch('http://localhost:3000/products');
+    if (!response.ok) {
+        throw new Error();
+    }
+    return await extractResponseData(response);
+}
+
