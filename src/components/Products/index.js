6f25f7ac4788431b9308edcc0d16dda8b9f890d2
pedrofn <pedrofechado@gmail.com>
2022-11-30 18:52:11 +0000

    <Products/> which makes the initial mount, iterates through products and displays data

---
+++ src/components/Products/index.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { fetchProducts } from '../../utils/apiRequests';
+import Product from '../Product/index';
+
+const Products = () => {
+    // state setter and getter
+    const [products, setProducts] = useState([]);
+
+    useEffect( () => {
+        fetchProducts()
+            .then( (productsData) => {
+                setProducts(productsData);
+            })
+            .catch((err) => {
+                err.message = 'Error! Could not resolve promise.';
+            });
+    }, []);
+    
+    return (
+        <>
+            {
+            products.map((product) => {
+                return (
+                    <Product product={product} key={product.id}/>
+                );
+            })
+            }
+        </>
+    );
+};
+
+export default Products;
