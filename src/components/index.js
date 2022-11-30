e010c0c9f0cf6cda9f3d711fc997e32e0acc11f9
pedrofn <pedrofechado@gmail.com>
2022-11-30 18:49:36 +0000

    index.js holding <Main/>

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Products from './Products/index';
+import Header from './Header/index';
+import Footer from './Footer/index';
+
+const Main = () => {
+    return (
+        <>
+            <Header />
+            <Products />
+            <Footer />
+        </>
+    );
+};
+
+export default Main;
+
