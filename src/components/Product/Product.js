0edb7eb74c672684e7be99231e374b4ccc143857
pedrofn <pedrofechado@gmail.com>
2022-12-01 10:45:08 +0000

    renamed files to be more semantic of what component it refers to

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,14 +1,27 @@
 const Product = (props) => {
-    let { id, title, price, image , category_id, category, character_id, character, description, image2, image3} = props.product
+    let {
+            id,
+            title,
+            price,
+            image ,
+            category_id,
+            category,
+            character_id,
+            character,
+            description,
+            image2,
+            image3
+        } = props.product;
+
     return (
-        <div id={id}>
+        <li id={id}>
             <h2>{title}</h2>
             <p>{price}</p>
             <img src={image} alt='product'/>
             <p className={category_id}>{category}</p>
             <p className={character_id}>{character}</p>
             <p>{description}</p>
-        </div>
+        </li>
     );
 };
 
