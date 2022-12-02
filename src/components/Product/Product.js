637f2e06929d6a2f09e1ff3c2028a74de720adf6
pedrofn <pedrofechado@gmail.com>
2022-12-02 14:35:56 +0000

    edited the currency format in price

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -11,7 +11,7 @@ const Product = (props) => {
             <a href="https://www.google.com">
                 <img src={image} alt='product'/>
                 <h3>{title}</h3>
-                <p>{price}</p>
+                <p>{price.toLocaleString(undefined, {style: 'currency', currency: 'GBP'})}</p>
             </a>
         </li>
     );
