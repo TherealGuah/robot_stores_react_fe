090e57c17f100d51d9e0de46848318265d371b3a
pedrofn <pedrofechado@gmail.com>
2022-12-02 17:02:44 +0000

    created api request for single product by id

--- src/utils/apiRequests.js
+++ src/utils/apiRequests.js
@@ -10,3 +10,11 @@ export const fetchProducts = async () => {
     return await extractResponseData(response);
 };
 
+export const fetchProduct = async (productId) => {
+    const response = await fetch(`http://localhost:3000/products/${productId}`);
+    if (!response.ok) {
+        throw new Error();
+    }
+    return await extractResponseData(response);
+};
+
