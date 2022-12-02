3369a18feca5104d566eb8cb20ee2acc7ba9ad49
pedrofn <pedrofechado@gmail.com>
2022-12-02 16:12:59 +0000

    base css for header banner created with responsiveness for mobile and desktop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,20 @@
 const Header = () => {
     return (
         <header id="header">
-            <nav>Navigation bar</nav>
+            <div className="header-wrapper">
+                <div className="header-text">
+                    <h1>Shop</h1>
+                    <p>Share your love of technology, and spread good cheer, with Binaryville official merchandise, including
+                        t-shirts, hats, mugs, aprons, posters, stickers, and more. Each unique treasure features a charming
+                        Binaryville robot, encapsulated inside a motivating historical quote, cleverly updated for the times.
+                        Even the most hard-to-please individuals can't resist these infectious designs. Perfect for the techie
+                        in your life!
+                    </p>
+                </div>
+                <div className="header-image">
+                    <img src="https://www.binaryville.com/images/product-montage.png"/>
+                </div>
+            </div>
         </header>
     );
 };
