c2fa40d69f9a44a553c9966621b5757e138bbd1c
pedrofn <pedrofechado@gmail.com>
2022-12-04 17:53:09 +0000

    made changes to the structure of the <form> in both components

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -5,12 +5,18 @@ const Categories = () => {
                 <input type="checkbox" id="aprons" name="aprons" />
                 <label htmlFor="bubbles">Aprons</label>
             </div>
-            <input type="checkbox" id="hats" name="hats" />
-            <label htmlFor="bubbles">Baseball Hats</label>
-            <input type="checkbox" id="mugs" name="mugs" />
-            <label htmlFor="bubbles">Mugs</label>
-            <input type="checkbox" id="shirts" name="shirts" />
-            <label htmlFor="bubbles">T-shirts</label>
+            <div>
+                <input type="checkbox" id="hats" name="hats" />
+                <label htmlFor="bubbles">Baseball Hats</label>
+            </div>
+            <div>
+                <input type="checkbox" id="mugs" name="mugs" />
+                <label htmlFor="bubbles">Mugs</label>
+            </div>
+            <div>
+                <input type="checkbox" id="shirts" name="shirts" />
+                <label htmlFor="bubbles">T-shirts</label>
+            </div>
         </form>
     );
 };
--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,14 +1,22 @@
 const Characters = () => {
     return (
         <form>
-            <input type="checkbox" id="bubbles" name="bubbles" />
-            <label htmlFor="bubbles">Bubbles</label>
-            <input type="checkbox" id="dolores" name="dolores" />
-            <label htmlFor="bubbles">Dolores</label>
-            <input type="checkbox" id="fred" name="fred" />
-            <label htmlFor="bubbles">Fred</label>
-            <input type="checkbox" id="rex" name="rex" />
-            <label htmlFor="bubbles">Rex</label>
+            <div>
+                <input type="checkbox" id="bubbles" name="bubbles" />
+                <label htmlFor="bubbles">Bubbles</label>
+            </div>
+            <div>
+                <input type="checkbox" id="dolores" name="dolores" />
+                <label htmlFor="bubbles">Dolores</label>
+            </div>
+            <div>
+                <input type="checkbox" id="fred" name="fred" />
+                <label htmlFor="bubbles">Fred</label>
+            </div>
+            <div>
+                <input type="checkbox" id="rex" name="rex" />
+                <label htmlFor="bubbles">Rex</label>
+            </div>
         </form>
     );
 };
