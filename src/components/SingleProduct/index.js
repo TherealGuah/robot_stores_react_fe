abdd6dc2707faa380baf0aaf3d9c809260dfd758
pedrofn <pedrofechado@gmail.com>
2022-12-05 18:21:01 +0000

    added api request for single Product

---
+++ src/components/SingleProduct/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const SingleProduct = () => {};
+
+export default SingleProduct;
--- src/utils/apiRequests.js
+++ src/utils/apiRequests.js
@@ -5,3 +5,12 @@ export const fetchProducts = async () => {
     }
     return await response.json();
 };
+
+// TODO: Hardcoded for now change to display dynamically
+export const fetchSingleProduct = async () => {
+    const response = await fetch('http://localhost:4000/products/1');
+    if (!response.ok) {
+        throw new Error();
+    }
+    return await response.json();
+};
