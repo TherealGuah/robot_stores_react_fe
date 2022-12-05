fd82cff71f7862e59069537a35040392c4d82cbc
pedrofn <pedrofechado@gmail.com>
2022-12-05 18:33:04 +0000

    created SingeProduct route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './app.css';
 import {Route, Routes} from "react-router-dom";
 import Body from "./components/Body";
 import PageNotFound from "./components/PageNotFound";
+import SingleProduct from "./components/SingleProduct";
 
 function App() {
   return (
     <>
         <Routes>
-            <Route path="/" element={<Body />} />
+            <Route path="/products" element={<Body />} />
+            <Route path="products/:productId" element={<SingleProduct />} />
             <Route path="*" element={<PageNotFound />} />
         </Routes>
     </>
