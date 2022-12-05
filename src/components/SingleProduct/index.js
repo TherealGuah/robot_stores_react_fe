0d9804956ac823fef64382d0cf2c660e8992ab6d
pedrofn <pedrofechado@gmail.com>
2022-12-05 18:51:44 +0000

    displayed dynamicaly SingleProduct

--- src/components/SingleProduct/index.js
+++ src/components/SingleProduct/index.js
@@ -1,30 +1,28 @@
 import React, {useState, useEffect} from 'react';
-import {fetchSingleProduct} from "../../utils/apiRequests";
+import { fetchSingleProduct } from "../../utils/apiRequests";
+import { useParams } from 'react-router-dom';
 
 const SingleProduct = () => {
     const [product, setProduct] = useState([]);
+    const {productId} = useParams();
 
     useEffect( () => {
-        fetchSingleProduct()
-            .then(product => setProduct(product))
+        fetchSingleProduct(productId)
+            .then(product => setProduct(product[0]))
             .catch(err => err.message = 'Error! Could not resolve promise.');
     }, []);
-    console.log(product)
+    console.log(product);
     const {
-        _id, id, title, price, image, category_id, category,
+         _id, id, title, price, image, category_id, category,
         character_id, character, description, image2, image3
-    } = product[0];
+    } = product;
 
     return (
-        <>
+        <div>
             <img src={image} alt="image of product"/>
             <h1>{title}</h1>
             <p>{description}</p>
-
-
-
-
-        </>
+        </div>
     );
 };
 
