22014f670fd6b56008bb6e6ade01f3f7266291d4
pedrofn <pedrofechado@gmail.com>
2022-12-05 18:33:57 +0000

    created fetch request to API to get the product by ID. Still hardcoded need to b done dynamically

--- src/components/SingleProduct/index.js
+++ src/components/SingleProduct/index.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {fetchSingleProduct} from "../../utils/apiRequests";
 
-const SingleProduct = () => {};
+const SingleProduct = () => {
+    const [product, setProduct] = useState([]);
+
+    useEffect( () => {
+        fetchSingleProduct()
+            .then(product => setProduct(product))
+            .catch(err => err.message = 'Error! Could not resolve promise.');
+    }, []);
+    console.log(product)
+    const {
+        _id, id, title, price, image, category_id, category,
+        character_id, character, description, image2, image3
+    } = product[0];
+
+    return (
+        <>
+            <img src={image} alt="image of product"/>
+            <h1>{title}</h1>
+            <p>{description}</p>
+
+
+
+
+        </>
+    );
+};
 
 export default SingleProduct;
