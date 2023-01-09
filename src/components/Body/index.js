a8cec1fa8bad9012ce22234dabf0e1dafcfa2a24
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-01-09 09:19:16 +0000

    moved <Body /> into its own component folder

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Products from './Products';
-import Header from './Header';
-import Footer from './Footer';
+import Products from '../Products';
+import Header from '../Header';
+import Footer from '../Footer';
 
 const Body = () => {
     return (
