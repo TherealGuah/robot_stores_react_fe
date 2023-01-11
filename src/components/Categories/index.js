df7ba35f4cbcb0bde96169071a1ef7f3cd66b889
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-01-11 16:58:04 +0000

    changed the default checked status to true

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -3,19 +3,19 @@ const Categories = ({handleAprons, handleHats}) => {
     return (
         <form>
             <div>
-                <input type="checkbox" id="aprons" name="aprons" onChange={handleAprons}/>
+                <input type="checkbox" id="aprons" name="aprons" onChange={handleAprons} checked={true}/>
                 <label htmlFor="bubbles">Aprons</label>
             </div>
             <div>
-                <input type="checkbox" id="hats" name="hats" onChange={handleHats}/>
+                <input type="checkbox" id="hats" name="hats" onChange={handleHats} checked={true}/>
                 <label htmlFor="bubbles">Baseball Hats</label>
             </div>
             <div>
-                <input type="checkbox" id="mugs" name="mugs" />
+                <input type="checkbox" id="mugs" name="mugs"  checked={true}/>
                 <label htmlFor="bubbles">Mugs</label>
             </div>
             <div>
-                <input type="checkbox" id="shirts" name="shirts" />
+                <input type="checkbox" id="shirts" name="shirts"  checked={true}/>
                 <label htmlFor="bubbles">T-shirts</label>
             </div>
         </form>
--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -2,19 +2,19 @@ const Characters = () => {
     return (
         <form>
             <div>
-                <input type="checkbox" id="bubbles" name="bubbles" />
+                <input type="checkbox" id="bubbles" name="bubbles"  checked={true}/>
                 <label htmlFor="bubbles">Bubbles</label>
             </div>
             <div>
-                <input type="checkbox" id="dolores" name="dolores" />
+                <input type="checkbox" id="dolores" name="dolores"  checked={true}/>
                 <label htmlFor="bubbles">Dolores</label>
             </div>
             <div>
-                <input type="checkbox" id="fred" name="fred" />
+                <input type="checkbox" id="fred" name="fred"  checked={true}/>
                 <label htmlFor="bubbles">Fred</label>
             </div>
             <div>
-                <input type="checkbox" id="rex" name="rex" />
+                <input type="checkbox" id="rex" name="rex"  checked={true}/>
                 <label htmlFor="bubbles">Rex</label>
             </div>
         </form>
