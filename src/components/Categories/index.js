260190c167b732665dfdcfe925badf2ae06bbe3b
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-01-20 17:21:29 +0000

    changed the base html for the forms

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,21 +1,20 @@
 const Categories = ({handleChange}) => {
-
     return (
         <form>
             <div>
-                <input type="checkbox" id="Aprons" name="Aprons" onChange={handleChange} defaultChecked={true} />
+                <input type="checkbox" id="Aprons" onChange={handleChange} defaultChecked={true} />
                 <label htmlFor="Aprons">Aprons</label>
             </div>
             <div>
-                <input type="checkbox" id="Hats" name="Hats" onChange={handleChange} defaultChecked={true} />
+                <input type="checkbox" id="Hats" onChange={handleChange} defaultChecked={true} />
                 <label htmlFor="Hats">Baseball Hats</label>
             </div>
             <div>
-                <input type="checkbox" id="Mugs" name="Mugs" onChange={handleChange} defaultChecked={true} />
+                <input type="checkbox" id="Mugs" onChange={handleChange} defaultChecked={true} />
                 <label htmlFor="Mugs">Mugs</label>
             </div>
             <div>
-                <input type="checkbox" id="Shirts" name="Shirts" onChange={handleChange} defaultChecked={true} />
+                <input type="checkbox" id="Shirts" onChange={handleChange} defaultChecked={true} />
                 <label htmlFor="Shirts">T-shirts</label>
             </div>
         </form>
--- src/components/Characters/index.js
+++ src/components/Characters/index.js
@@ -1,22 +1,20 @@
-const Characters = (props) => {
-    const { handleChange } = props;
-
+const Characters = ({handleChange}) => {
     return (
         <form>
             <div>
-                <input type="checkbox" id="Bubbles" name="Bubbles" onChange={handleChange} defaultChecked={true}/>
+                <input type="checkbox" id="Bubbles" onChange={handleChange} defaultChecked={true}/>
                 <label htmlFor="Bubbles">Bubbles</label>
             </div>
             <div>
-                <input type="checkbox" id="Dolores" name="Dolores" onChange={handleChange} defaultChecked={true} />
+                <input type="checkbox" id="Dolores" onChange={handleChange} defaultChecked={true} />
                 <label htmlFor="bubbles">Dolores</label>
             </div>
             <div>
-                <input type="checkbox" id="Fred" name="Fred" onChange={handleChange} defaultChecked={true} />
+                <input type="checkbox" id="Fred" onChange={handleChange} defaultChecked={true} />
                 <label htmlFor="Fred">Fred</label>
             </div>
             <div>
-                <input type="checkbox" id="Rex" name="Rex" onChange={handleChange} defaultChecked={true} />
+                <input type="checkbox" id="Rex" onChange={handleChange} defaultChecked={true} />
                 <label htmlFor="Rex">Rex</label>
             </div>
         </form>
