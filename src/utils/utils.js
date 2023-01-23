4ae7feb30e98855446304aeb9e288b2a60396e43
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-01-23 13:35:07 +0000

    created function to filter categories

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,31 @@
 
+export const filterProducts = (productsToFilter, categories) => {
+    let filteredProducts = productsToFilter;
+
+    if (!categories['Aprons']) {
+        filteredProducts = filteredProducts.filter(product => product.category_id !== 1);
+    }
+    if (!categories['Hats']) {
+        filteredProducts = filteredProducts.filter(product => product.category_id !== 2);
+    }
+    if (!categories['Mugs']) {
+        filteredProducts = filteredProducts.filter(product => product.category_id !== 3);
+    }
+    if (!categories['Shirts']) {
+        filteredProducts = filteredProducts.filter(product => product.category_id !== 4);
+    }
+    if (!categories['Fred']) {
+        filteredProducts = filteredProducts.filter(product => product.character_id !== 1);
+    }
+    if (!categories['Bubbles']) {
+        filteredProducts = filteredProducts.filter(product => product.character_id !== 2);
+    }
+    if (!categories['Dolores']) {
+        filteredProducts = filteredProducts.filter(product => product.character_id !== 3);
+    }
+    if (!categories['Rex']) {
+        filteredProducts = filteredProducts.filter(product => product.character_id !== 4);
+    }
+
+    return filteredProducts;
+}
