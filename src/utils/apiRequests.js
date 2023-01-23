753262fcb14bf85cf1bf2eaf1d98f958a703970f
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-01-23 13:45:07 +0000

    removed TODO: comment

--- src/utils/apiRequests.js
+++ src/utils/apiRequests.js
@@ -6,7 +6,6 @@ export const fetchProducts = async () => {
     return await response.json();
 };
 
-// TODO: Hardcoded for now change to display dynamically
 export const fetchSingleProduct = async (productId) => {
     const response = await fetch(`http://localhost:4000/products/${productId}`);
     if (!response.ok) {
