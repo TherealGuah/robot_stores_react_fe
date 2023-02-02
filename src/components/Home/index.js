44429360587864dfbfc830ba1e9b5a0a1a1e8878
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-02-02 18:45:00 +0000

    styled home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,18 @@
 import {Link} from 'react-router-dom';
 import React from "react";
-import {ReactComponent as RobotLogo} from './robot_logo.svg';
+import {ReactComponent as RobotLogo} from './logo_light.svg';
 const Home = () => {
 
     // TODO: Style Home Page
     return (
-        <>
+        <div id="home">
             <h1>Welcome to Robots Store</h1>
             <RobotLogo />
             <img src="https://www.binaryville.com/images/product-montage.png" alt="Robot Store Merchandise"/>
             <Link to={'/products'}>
-                <h1>Enter Store</h1>
+                <h2>Enter Store</h2>
             </Link>
-        </>
+        </div>
     );
 };
 
