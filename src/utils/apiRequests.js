a7ecf118ea972f07c4c7b47115b581b58cefc0bf
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2023-02-23 17:12:47 +0000

    installed Jest dependency and updated the API endpoint call

--- src/utils/apiRequests.js
+++ src/utils/apiRequests.js
@@ -1,5 +1,7 @@
+const endpoint = 'https://robotapi.2022-pedron.dev.io-academy.uk';
+
 export const fetchProducts = async () => {
-    const response = await fetch('http://localhost:4000/products');
+    const response = await fetch(`${endpoint}/products`);
     if (!response.ok) {
         throw new Error();
     }
@@ -7,7 +9,7 @@ export const fetchProducts = async () => {
 };
 
 export const fetchSingleProduct = async (productId) => {
-    const response = await fetch(`http://localhost:4000/products/${productId}`);
+    const response = await fetch(`${endpoint}/products/${productId}`);
     if (!response.ok) {
         throw new Error();
     }
